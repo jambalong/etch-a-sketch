59026e5d3da3cf55a77cbaee9f0dc7a514666f85
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-05-28 15:21:04 -0700

    Define createSquares() to create the 16x16 grid of squares

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let container = document.querySelector("#container");
 
-let square = document.createElement("div");
-square.setAttribute("class", "square");
+function createSquares() {
+  let container = document.querySelector("#container");
 
-for (i = 0; i < 256; i++) {
-  container.appendChild(square.cloneNode(true))
+  for (i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.setAttribute("class", "square");
+
+    container.appendChild(square);
+  }
 }
+
+createSquares();
